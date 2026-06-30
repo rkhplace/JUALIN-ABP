d118f1d2f27dfb255899ce13f528aee6808f376f
rkhplace <mrakhaptatama135@gmail.com>
2026-06-30 15:26:36 +0700

    Unify page backgrounds with login

--- jualin/src/components/ui/DashboardBackground.jsx
+++ jualin/src/components/ui/DashboardBackground.jsx
@@ -1,16 +1,6 @@
 import React from "react";
+import AuthBackground from "./AuthBackground.jsx";
 
 export default function DashboardBackground() {
-  return (
-    <div className="dashboard-background" aria-hidden="true">
-      <span className="dashboard-bg-red dashboard-bg-red-left" />
-      <span className="dashboard-bg-red dashboard-bg-red-right" />
-
-      <span className="dashboard-bg-paper dashboard-bg-paper-top" />
-      <span className="dashboard-bg-paper dashboard-bg-paper-left" />
-      <span className="dashboard-bg-paper dashboard-bg-paper-middle" />
-      <span className="dashboard-bg-paper dashboard-bg-paper-right" />
-      <span className="dashboard-bg-paper dashboard-bg-paper-bottom" />
-    </div>
-  );
+  return <AuthBackground className="dashboard-background" />;
 }
